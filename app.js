a1c43c2efe42b676084116040e45808f6501bad4
Olive <olivelliott48@gmail.com>
2022-06-05 18:43:46 -0400

    Generate HTML based on input

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-}
-
-profileDataArr.forEach(profileItem => console.log(profileItem));
-
-printProfileData(profileDataArgs);
-=======
 const fs = require('fs');
 const generatePage = require('./src/page-template.js')
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const [name, github] = profileDataArgs;
-
 
 fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
@@ -42,4 +27,3 @@ fs.writeFile('index.html', generatePage(name, github), err => {
 // profileDataArr.forEach(profileItem => console.log(profileItem));
 
 // printProfileData(profileDataArgs);
->>>>>>> feature/generate-webpage
