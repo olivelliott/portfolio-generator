3f36eb6306a53a3b5e183dcce301619aab846197
Olive <olivelliott48@gmail.com>
2022-06-08 22:53:06 -0400

    Handle asynchronous functionality with generate-site.js and refactor code in app.js

--- src/page-template.js
+++ src/page-template.js
@@ -54,61 +54,6 @@ const generateProjects = projectsArr => {
     `;
 };
 
-//     const featuredProjects = projectsArr.filter(project => {
-//         if (project.feature) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     });
-
-//     const nonFeaturedProjects = projectsArr.filter(project => {
-//         if (!project.feature) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     });
-
-//     const featuredProjectHtmlArr = featuredProjects.map(({name, description, languages, link}) => {
-//         return `
-//             <div class='col 12 mb-2 bg-dark text-light p-3 flex-column'>
-//                 <h3 class='portfolio-item-title text-light'>${name}</h3>
-//                 <h5 class='portfolio-languages'>
-//                     Built With:
-//                         ${languages.join(', ')}
-//                 </h5>
-//                 <p>${description}</p>
-//                 <a href='${link}' class='btn mt-auto'><i class='fab fa-github mr-2'></i>View Project on GitHub</a>
-//             </div>
-//         `;
-//     });
-
-//     const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({name, description, languages, link}) => {
-//         return `
-//             <div class='col 12 col-md-6 mb-2 bg-dark text-light p-3 flex-column'>
-//                 <h3 class='portfolio-item-title text-light'>${name}</h3>
-//                 <h5 class='portfolio-languages'>
-//                     Built With:
-//                         ${languages.join(', ')}
-//                 </h5>
-//                 <p>${description}</p>
-//                 <a href='${link}' class='btn mt-auto'><i class='fab fa-github mr-2'></i>View Project on GitHub</a>
-//             </div>
-//         `;
-//     });
-
-//     return `
-//         <section class='my-3' id='portfolio'>
-//             <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-//             <div class="flex-row justify-space-between">
-//                 ${featuredProjectHtmlArr.join(' ')}
-//                 ${nonFeaturedProjectHtmlArr.join(' ')}
-//             </div>
-//         </section>
-//     `;
-// };
-
 module.exports = templateData => {
     const {projects, about, ...header} = templateData;
     return `
@@ -118,6 +63,7 @@ module.exports = templateData => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel='stylesheet' type='text/css' href='./style.css'>
         <title>Portfolio Demo</title>
     </head>
     <body>
